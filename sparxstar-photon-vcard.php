3ad51e4bc33bf897d468eb471f0a1ad563a02673
Max Barrett <34328348+MaximillianGroup@users.noreply.github.com>
2026-02-20 19:51:49 -0800

    Update plugin header information and version
    Signed-off-by: Max Barrett <34328348+MaximillianGroup@users.noreply.github.com>

--- sparxstar-photon-vcard.php
+++ sparxstar-photon-vcard.php
@@ -1,19 +1,25 @@
 <?php
 /**
- * Plugin Name: SPARXSTAR Photon VCard
- * Plugin URI: https://github.com/Starisian-Technologies/sparxstar-photon-vcard
- * Description: A secure, accessible, and resilient digital business card overlay that can be triggered via device motion or touch.
- * Version: 1.0.0
- * Requires at least: 6.8
- * Requires PHP: 8.2
- * Author: Starisian Technologies
- * Author URI: https://starisian.tech
- * License: Proprietary
- * License URI: https://github.com/Starisian-Technologies/sparxstar-photon-vcard/blob/main/LICENSE.md
- * Text Domain: sparxstar-photon-vcard
- * Domain Path: /languages
+ * SPARXSTAR Photon VCard
+ * @version           0.5.0
+ * @package           sparxstar-photon-vcard
+ * @author            Starisian Technologies (Max Barrett) <support@starisian.com>
+ * @copyright         2025 Starisian Technologies. All rights reserved.
+ * @license           Starisian Technologies Proprietary
  *
- * @package SparxstarPhotonVcard
+ * @wordpress-plugin
+ * Plugin Name:       SPARXSTAR Photon VCard
+ * Plugin URI:        https://starisian.com/sparxstar/sparxstar-photon-vcard
+ * Description:       roduction-grade digital business card with motion/touch triggers. Finalized for accessibility, security, and legacy hardware resilience.
+ * Version:           0.5.0
+ * Requires at least: 6.8
+ * Requires PHP:      8.2
+ * Author:            Starisian Technologies (Max Barrett) <support@starisian.com>
+ * Author URI:        https://starisian.com
+ * Text Domain:       sparxstar-photon-vcard
+ * License:           Starisian Technologies Proprietary
+ * License URI: 	  https://github.com/Starisian-Technologies/sparxstar-photon-vcard/blob/main/LICENSE.md
+ * Update URI:        https://starisian.com/sparxstar/sparxstar-photon-vcard/update
  */
 
 // If this file is called directly, abort.
